70470d5b4ec35f76a9d7cd558fcdfafd05fd8dcf
EC2 Default User <ec2-user@ip-172-31-49-23.ec2.internal>
2021-07-14 06:41:17 +0000

    　デバイス以外のルーティング完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,38 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+     # devise_for :customers
+     
+
+   # admin
+     namespace :admin do
+      # 商品のルーティング
+      resources :items, only: [:new, :create, :edit, :update, :index, :show]
+     # 注文のルーティング
+      resources :orders, only: [:index, :show, :update]
+     # 注文商品のルーティング
+      resources :order_items, only: [:update]
+     # 顧客情報
+      resources :customers, only: [:index, :show, :edit, :update]
+     # ジャンル
+      resources :genres, only: [:index, :create, :edit, :update]
+     end
+     
+     
+   #customers
+   
+   # 商品のルーティング
+     root to: 'items#top'
+     get '/about' => 'items#about'
+     resources :items, only: [:index, :show]
+     
+     # カートのルーティング
+     resources :cart_items, only: [:index, :create, :update, :destroy]
+     delete '/all_destroy' => 'cart_items#all_destroy'
+     
+     # 注文のルーティング
+     resources :orders, only: [:new, :create, :index, :show]
+     post 'orders/confirm' => 'orders#confirm'
+     get 'orders/done' => 'orders#done'
+     
+     # 配送先のルーティング
+     resources :locations, only: [:index, :create, :edit ,:update, :destroy]
 end
