9332b83f07de55f706248ecbf30184483ae2898d
EC2 Default User <ec2-user@ip-172-31-47-95.ec2.internal>
2021-07-14 09:02:47 +0000

    [Add]moved

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  
- 
+
+
 # デバイスのカスタマーのルーティング
    devise_for :customers
-     
+
 
    # 管理者　admin
-     namespace :admin do 
+     namespace :admin do
      # 管理者のセッション管理
       devise_for :admins, class_name: "Admin::Admin"
       # 商品のルーティング
@@ -20,29 +20,31 @@ Rails.application.routes.draw do
      # ジャンル
       resources :genres, only: [:index, :create, :edit, :update]
      end
-     
-     
+
+
    #顧客　customers
-   
+
    # 商品のルーティング
+   scope module: :public do
      root to: 'items#top'
      get '/about' => 'items#about'
      resources :items, only: [:index, :show]
-     
+
      # カートのルーティング
      resources :cart_items, only: [:index, :create, :update, :destroy]
      delete '/all_destroy' => 'cart_items#all_destroy'
-     
+
      # 注文のルーティング
      resources :orders, only: [:new, :create, :index, :show]
      post 'orders/confirm' => 'orders#confirm'
      get 'orders/done' => 'orders#done'
-     
+
      # 顧客情報
      resources :customers, only: [:show, :edit, :update]
      get 'customers/unsubscribe' => 'customers#unsubscribe'
      patch 'customers/withdraw' => 'customers#withdraw'
-     
+
      # 配送先のルーティング
      resources :locations, only: [:index, :create, :edit ,:update, :destroy]
+   end
 end
