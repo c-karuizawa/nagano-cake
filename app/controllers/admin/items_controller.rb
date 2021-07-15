2536e24b5aef1d58ad0f400651019327bddecb94
EC2 Default User <ec2-user@ip-172-31-46-137.ec2.internal>
2021-07-15 12:09:44 +0000

    商品投稿一覧詳細画面実装

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,20 +1,36 @@
 class Admin::ItemsController < ApplicationController
   def new
+    @item = Item.new
   end
 
   def create
+    @item=Item.new(item_params)
+    if @item.save
+    redirect_to admin_item_path(@item)
+    else
+    render :new
+    end
   end
 
   def edit
+    @item=Item.find(params[:id])
   end
 
   def update
   end
 
   def index
+    @items=Item.page(params[:page]).reverse_order
   end
 
   def show
+    @item=Item.find(params[:id])
+  end
+  
+  private
+  def item_params
+    params.require(:item).permit(:image,:name,:introduction,:price)
+    # ジャンル、販売ステータスについては未設定です
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_14_065808) do
+ActiveRecord::Schema.define(version: 2021_07_15_040245) do
 
   create_table "admin_admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -34,6 +34,17 @@ ActiveRecord::Schema.define(version: 2021_07_14_065808) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer "genre_id"
+    t.string "name"
+    t.text "introduction"
+    t.integer "price"
+    t.string "image_id"
+    t.boolean "on_sale"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
