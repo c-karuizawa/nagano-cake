f5c73e8be9f1749720b864cc6ecdcda5a969ba5d
EC2 Default User <ec2-user@ip-172-31-47-95.ec2.internal>
2021-07-15 13:29:56 +0000

    finish edit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# メインのサンプルユーザーを1人作成する
+Customer.create!(last_name:  "山田",
+             first_name: "花子",
+             last_name_kana: "ヤマダ",
+             first_name_kana: "ハナコ",
+             postal_code: "1234567",
+             address: "スカイツリー",
+             phone_number: "09012345678",
+             email: "hanako@email",
+             password: "foobar",
+             password_confirmation: "foobar")
+
+# # 追加のユーザーをまとめて生成する
+# 99.times do |n|
+#   name  = Faker::Name.name
+#   email = "example-#{n+1}@railstutorial.org"
+#   password = "password"
+#   User.create!(name:  name,
+#               email: email,
+#               password:              password,
+#               password_confirmation: password)
+# end
