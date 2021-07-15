553c2ae9d3a62f4d89ecbff6ddac7c3f9ba08644
EC2 Default User <ec2-user@ip-172-31-47-95.ec2.internal>
2021-07-15 13:38:53 +0000

    public/customers/edit画面を実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,14 +16,3 @@ Customer.create!(last_name:  "山田",
              email: "hanako@email",
              password: "foobar",
              password_confirmation: "foobar")
-
-# # 追加のユーザーをまとめて生成する
-# 99.times do |n|
-#   name  = Faker::Name.name
-#   email = "example-#{n+1}@railstutorial.org"
-#   password = "password"
-#   User.create!(name:  name,
-#               email: email,
-#               password:              password,
-#               password_confirmation: password)
-# end
