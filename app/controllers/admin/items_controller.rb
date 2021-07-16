29d9b1e8eaf05001a6fdbcfe37470e4fb2ed95e9
kei-i-wa <kei.kumarg@gmail.com>
2021-07-16 13:38:17 +0000

    FIX 商品編集不具合修正

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -17,6 +17,7 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
+    @item=Item.find(params[:id])
     if @item.update(item_params)
     redirect_to admin_item_path(@item)
     else
