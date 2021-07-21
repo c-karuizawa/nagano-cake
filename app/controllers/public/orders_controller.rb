00e73e47d79d954791eb6214056077a9cd24e4ca
EC2 Default User <ec2-user@ip-172-31-47-95.ec2.internal>
2021-07-21 15:11:20 +0000

    [Orders]fixed layout

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -9,29 +9,33 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
-    customer = current_customer
-    @cart_items = customer.cart_items
+    @customer = current_customer
+    @cart_items = @customer.cart_items
     @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
-    @locations = customer.locations
+    @locations = @customer.locations
     @order = Order.new(@attr)
     session[:order] = @order
     if params[:locate] == "0"
-      session[:order][:postal_code] = customer.postal_code
-      session[:order][:address] = customer.address
-      session[:order][:name] = customer.last_name + customer.first_name
-      session[:order][:customer_id] = customer.id
+      session[:order][:postal_code] = @customer.postal_code
+      session[:order][:address] = @customer.address
+      session[:order][:name] = @customer.last_name + @customer.first_name
+      session[:order][:customer_id] = @customer.id
       session[:order][:total_price] = @total + @order.postage
     elsif params[:locate] == "1"
       location = @locations.find(params[:order][:locate_id])
       session[:order][:postal_code] = location.postal_code
       session[:order][:address] = location.address
       session[:order][:name] = location.name
-      session[:order][:customer_id] = customer.id
+      session[:order][:customer_id] = @customer.id
       session[:order][:total_price] = @total + @order.postage
     elsif params[:locate] == "2"
-      @order = Order.new(@attr)
-      session[:order][:customer_id] = customer.id
-      session[:order][:total_price] = @total + @order.postage
+      if session[:order][:postal_code].present? && session[:order][:address].present? && session[:order][:name].present?
+        @order = Order.new(@attr)
+        session[:order][:customer_id] = @customer.id
+        session[:order][:total_price] = @total + @order.postage
+      else
+        redirect_to new_order_path, alert: 'お届け先を選択または入力してください'
+      end
     end
   end
 
@@ -39,15 +43,12 @@ class Public::OrdersController < ApplicationController
     order = Order.new(session[:order])
     order.save
     @cart_items = current_customer.cart_items.all
-    puts "-----------------------"
-    p order
-    puts "-----------------------"
     @cart_items.each do |cart_item|
       OrderItem.create!(order_id: order.id,
                         item_id: cart_item.item_id,
                         quantity: cart_item.quantity,
                         taxin_price: cart_item.item.taxin_price
-                      )
+                       )
     end
     session.delete(:order)
     @cart_items.destroy_all
@@ -66,12 +67,8 @@ class Public::OrdersController < ApplicationController
     customer = current_customer
     orders = customer.orders
     @order = orders.find(params[:id])
-
     order_items = @order.order_items
     @total = order_items.inject(0) { |sum, item| sum + item.sum_of_price }
-
-    # @total = order_items.inject(0) { |sum, item| sum + item.sum_of_price }
-
   end
 
   private
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,9 @@ class Order < ApplicationRecord
   belongs_to :customer, optional: true
   has_many :order_items
 
+  validates :postal_code, :address, :name, presence: true
+
+
   # enum payment: { card: 0, bank: 1 }
   # enum order_status: { waiting: 0, confirmed: 1, making: 2, preparing: 3, shipped: 4 }
   enum payment: { "クレジットカード": 0, "銀行振込": 1 }
